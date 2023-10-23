03f8fc8de1b80028cc84d85d26257041f85e5941
shufayang <1214792408@qq.com>
2023-10-23 14:50:30 +0800

    data abort问题通过降低交叉编译版本解决，否则导致其他问题，例如tftp启动kernel出现的data abort

--- uboot_emmc.sh
+++ uboot_emmc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-  mx6ull_alientek_emmc_defconfig
-make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j16
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-  mx6ull_alientek_emmc_defconfig
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
